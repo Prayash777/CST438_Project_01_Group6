f05dd955be4b4f09f17e0b24fe4d996bbb2155ce
CSnoah <143917634+CSnoah@users.noreply.github.com>
2025-02-05 11:49:21 -0800

    Added a database,insert query,and loging query, and sign up functionality

--- habit-tracker/app/_layout.tsx
+++ habit-tracker/app/_layout.tsx
@@ -1,4 +1,4 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { DarkTheme, DefaultTheme, ThemeProvider, NavigationContainer } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
@@ -6,10 +6,35 @@ import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 
+// database import
+import { SQLiteDatabase, SQLiteProvider } from "expo-sqlite";
+
 import { useColorScheme } from '@/hooks/useColorScheme';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
+
+// creating the sqlite database
+const createDbIfNeeded = async (db: SQLiteDatabase) => {
+  console.log("Creating database");
+  try {
+    let response;
+
+    // for developement purposes to get a clean table
+    // comment out to save tables per expo app start
+    // let response = await db.execAsync(
+    //   "DROP TABLE IF EXISTS users"
+    // );
+
+    // Create a table
+    response = await db.execAsync(
+      "CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, email TEXT, password TEXT)"
+    );
+    console.log("Database created", response);
+  } catch (error) {
+    console.error("Error creating database:", error);
+  }
+};
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
@@ -29,12 +54,27 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        {/* Why is this not working */}
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} /> 
-        <Stack.Screen name="+not-found" />
-      </Stack>
+      <SQLiteProvider databaseName="test.db" onInit={createDbIfNeeded}>
+        <Stack>
+          <Stack.Screen name="auth/Login" />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+      </SQLiteProvider>
       <StatusBar style="auto" />
     </ThemeProvider>
   );
+
+  // return (
+  //   <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+  //     <SQLiteProvider databaseName="test.db" onInit={createDbIfNeeded}>
+  //       <Stack>
+  //         {/* Why is this not working */}
+  //         {/* <Stack.Screen name="(tabs)" options={{ headerShown: false }} /> */}
+  //         <Stack.Screen name="./auth/Login" />
+  //         <Stack.Screen name="+not-found" />
+  //       </Stack>
+  //     </SQLiteProvider>
+  //     <StatusBar style="auto" />
+  //   </ThemeProvider>
+  // );
 }
